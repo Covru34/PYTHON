fee897a20978e4528de9b047a819e1501136872f
Covru34 <mikolamihalcuk01@gmail.com>
2026-02-20 17:44:36 +0200

    did task 3-4 in dz15

--- python_dz/dz.15.py
+++ python_dz/dz.15.py
@@ -16,14 +16,50 @@ writeInFiel()
 
 # Завдання 2
 
+# try:
+#     with open("data.txt", "r", encoding="utf-8") as my:
+#         lines = my.readlines() 
+
+#         print("++Кожен другий рядок файлу data.txt++")
+
+#         for i in range(1, len(lines), 2): 
+#             print(lines[i].strip())
+
+# except:
+#     print("--Помилка роботи із файлом--")
+
+# Завдання 3
+
 try:
-    with open("data.txt", "r", encoding="utf-8") as my:
-        lines = my.readlines() 
+    with open("data.txt", "r", encoding="utf-8") as file:
+        lines = file.readlines()
 
-        print("++Кожен другий рядок файлу data.txt++")
+    with open("filtered.txt", "w", encoding="utf-8") as new_file:
+        for line in lines:
+            if "Python" in line:
+                new_file.write(line)
 
-        for i in range(1, len(lines), 2): 
-            print(lines[i].strip())
+    print("Рядки з словом 'Python' записані у filtered.txt")
 
-except:
-    print("--Помилка роботи із файлом--")
+except FileNotFoundError:
+    print("Файл data.txt не знайдено!")
+except Exception as e:
+    print("Сталася помилка:", e)
+
+# Завдання 4
+
+try:
+    fielName = input("Введіть ім'я файлу: ")
+
+    with open(fielName, "r", encoding="utf-8") as fiel:
+        content = fiel.read()
+
+    cleaned_content = "".join(char for char in content if not char.isdigit())
+
+    with open("cleaned.txt", "w", encoding="utf-8") as new_file:
+        new_file.write(cleaned_content)
+
+    print("Готово")
+
+except FileNotFoundError:
+    print("Файл не знайдено")
